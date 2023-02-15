f96e299a1fa64592802c89b3aa957ee30de5bf40
Luis Mogoyan <cbi2163009196@izt.uam.mx>
2023-02-14 22:42:55 -0600

    Primer programa de prueba con haskell

---
+++ prueba.hs
@@ -0,0 +1,9 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Redundant bracket" #-}
+tuFechaDeNacimiento = 1987
+
+tuNombre = "Luis"
+
+doble x = 2 * x
+
+dobleXmasUno x = doble x + 1   
